4620e91217f54a21aa65b05c12492118470d4538
Leandro <leandrosantiagoa@gmail.com>
2019-11-09 14:17:19 -0300

    add state to save data in json file.

---
+++ robots/input.js
@@ -0,0 +1,24 @@
+const readline = require('readline-sync');
+const state = require('./state.js');
+
+function robot() {
+  const content = {
+    maximumSentences: 7
+  };
+  content.searchTerm = askAndReturnSearchTerm();
+  content.prefix = askAndReturnPrefix();
+  state.save(content);
+
+  function askAndReturnSearchTerm() {
+    return readline.question("Type a Wikepedia search term: ");
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ["Who is", "What is", "The history of"];
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, "Choose one option: ");
+
+    return prefixes[selectedPrefixIndex];
+  }
+}
+
+module.exports = robot;
