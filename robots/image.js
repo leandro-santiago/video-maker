269493f71cb4be05ceb14a8932d2ceb8c0e5dc23
Leandro <leandrosantiagoa@gmail.com>
2019-11-13 14:35:54 -0300

    add download and save image logic

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const {google} = require('googleapis');
 const customSearch = google.customsearch('v1');
 const state = require('./state.js');
@@ -6,9 +7,9 @@ const googleSearchCredentials = require('../credentials/google-search.json');
 async function robot() {
   const content = state.load();
 
-  await fetchImagesOfAllSentences(content);
-
-  state.save(content);
+  //await fetchImagesOfAllSentences(content);
+  await downloadAllImages(content);
+  //state.save(content);
   
   async function fetchImagesOfAllSentences(content) {
     for (const sentence of content.sentences) {
@@ -33,6 +34,38 @@ async function robot() {
     });
 
     return imagesUrl;
+  }
+
+  async function downloadAllImages(content) {
+    content.downloadedImages = [];
+    
+    for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+      const images = content.sentences[sentenceIndex].images;
+
+      for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+        const imageUrl = images[imageIndex];
+
+        try {
+          if (content.downloadedImages.includes(imageUrl)) {
+            throw new Error("Image already downloaded");
+          }
+
+          await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
+          content.downloadedImages.push(imageUrl);
+          console.log(`> [${sentenceIndex}] [${imageIndex}] Image downloaded with success: ${imageUrl}`);
+          break;
+        } catch (error) { 
+          console.log(`> [${sentenceIndex}] [${imageIndex}] Error to download (${imageUrl}): ${error}`);
+        }
+      }
+    }
+  }
+
+  async function downloadAndSave(url, fileName) {
+    return imageDownloader.image({
+      url, url,
+      dest: `./content/${fileName}`
+    });
   }
 }
 
